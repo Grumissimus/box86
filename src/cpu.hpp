45ce14930b4a7ba19c4a26cece811982c9e3d72b
Grumissimus <55281603+Grumissimus@users.noreply.github.com>
2019-09-13 22:11:13 +0200

    Separating CPU from Intel CPU, extending memory_unit.hpp

--- src/cpu.hpp
+++ src/cpu.hpp
@@ -1,55 +1,5 @@
-#include <stack>
-#include <iostream>
-#include <type_traits>
-
 #ifndef __CPU_HPP__
 #define __CPU_HPP__
-enum Register{
-	EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI
-};
-
-enum SegmentRegister{
-	CS, DS, SS, ES, FS, GS
-};
-
-enum Flags{
-	CF = 1,
-	PF = 1 << 2,
-	AF = 1 << 4,
-	ZF = 1 << 6,
-	SF = 1 << 7,
-	TF = 1 << 8,
-	IF = 1 << 9,
-	DF = 1 << 10,
-	OF = 1 << 11,
-	IOPL = (1 << 12) | (1 << 13),
-	NT = 1 << 14,
-	#ifndef __16_BIT_ONLY__
-	RF = 1 << 16,
-	VM = 1 << 17,
-	AC = 1 << 18,
-	VIF = 1 << 19,
-	VIP = 1 << 20,
-	ID = 1 << 21
-	#endif
-};
-
-enum IntelCPUType{
-	Intel8086,
-	Intel80186,
-	Intel80286,
-	Intel80386,
-	Intel80486,
-	IntelPentium,
-	IntelPentiumMMX,
-	IntelPentiumPro,
-	IntelPentiumII,
-	IntelPentiumIII,
-	IntelPentium4,
-	IntelCore,
-	IntelCore2,
-	IntelCorei7	
-};
 
 template <typename RegDataType, unsigned int RegAmount>
 class CPU{
@@ -57,37 +7,8 @@ class CPU{
 		RegDataType generalRegisters[RegAmount+1] = {0};
 		RegDataType flags = 0;
 	public:
-		virtual ~CPU() {};
-		virtual void printRegisters(){};
-};
-
-template<typename RegDataType = unsigned int, unsigned int RegAmount = 8>
-class IntelCPU : public CPU<RegDataType, RegAmount>{
-	static_assert(RegAmount >= 8, "A number of registers in Intel CPU cannot be lower than 8.");
-	private:
-		IntelCPUType type;
-	public:
-		IntelCPU() : type(Intel8086) {};
-		IntelCPU(IntelCPUType t) : type(t) {};
-		~IntelCPU(){};
-
-		void printRegisters(){
-			std::cout << "Register A: " << this->generalRegisters[EAX] << std::endl;
-			std::cout << "Register B: " << this->generalRegisters[EBX] << std::endl;
-			std::cout << "Register C: " << this->generalRegisters[ECX] << std::endl;
-			std::cout << "Register D: " << this->generalRegisters[EDX] << std::endl;
-			std::cout << "Stack Register: " << this->generalRegisters[ESP] << std::endl;
-			std::cout << "Base Pointer: " << this->generalRegisters[EBP] << std::endl;
-			std::cout << "Source Index: " << this->generalRegisters[ESI] << std::endl;
-			std::cout << "Destination Index: " << this->generalRegisters[EDI] << std::endl;
-			
-			if(RegAmount > 8){
-				for(unsigned int i = 9; i <= RegAmount; i++){
-					std::cout << "General Register " << i << ":" << this->generalRegisters[i] << std::endl;
-				}
-			}
-			
-		}
+		virtual ~CPU() {}
+		virtual void printRegisters(){}
 };
 
 #endif //__CPU_HPP__
--- src/memory_unit.hpp
+++ src/memory_unit.hpp
@@ -1,36 +1,54 @@
-#include <memory> /* memory in memory, oh my */
+#include <memory>
 #include <stdexcept>
+#include <cstring>
 #ifndef __MEMORY_UNIT_HPP__
 #define __MEMORY_UNIT_HPP__
 
-template<typename T = int>
 class Memory{
 	private:
-		std::unique_ptr<T[]> *space;
+		std::unique_ptr<unsigned char[]> space;
 		unsigned long long max_addr;
 	public:
 		Memory(unsigned long long size){
 			this->max_addr = size;
-			this->space = std::make_unique<T[]>(size);
+			this->space = std::make_unique<unsigned char[]>(size);
+			
+			for(unsigned int i = 0; i < size; i++){
+				this->space[i] = 0;
+			}
 		}
 		
 		~Memory(){
 		}
 		
-		template<typename U = T>
-		U read(unsigned int address, unsigned int numberOfBytes = sizeof(U)){
-			if( (address > this->max_addr) || (address+numberOfBytes > this->max_addr) ){
-				throw std::out_of_range("Invalid address");
+		template<typename U>
+		U read(unsigned long long destination, unsigned int numberOfBytes = sizeof(U)){
+			if( (destination > this->max_addr) || (destination+numberOfBytes > this->max_addr) ){
+				throw std::out_of_range("Error: Invalid address when reading value from the memory.");
 			}
-			U valueToReturn;
-								
-			std::memcpy((void*)valueToReturn, (void*)(this->space[address]), numberOfBytes);
 			
+			U valueToReturn = 0;
+														
+			std::memcpy(
+				(void*)(&valueToReturn), 
+				(void*)(&this->space[destination]), 
+				(sizeof(valueToReturn) < numberOfBytes) ? sizeof(valueToReturn) : numberOfBytes
+			);
+			
+			return valueToReturn;
 		}
 		
-		template<typename U = T>
-		void write(U value, T &destination){
+		template<typename U>
+		void write(unsigned long long destination, U value){
+			if( (destination > this->max_addr) || (destination+sizeof(value) > this->max_addr) ){
+				throw std::out_of_range("Error: Invalid address when writing value to the memory.");
+			}
 			
+			std::memcpy(
+				(void*)(&this->space[destination]), 
+				(void*)(&value), 
+				sizeof(value)
+			);
 		}
 };
 
